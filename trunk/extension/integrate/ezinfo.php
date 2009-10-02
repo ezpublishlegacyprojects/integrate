82cf72e413ee3db0e83f01b03dba79b389b61e8e
Gaetano Giunta <gg@ez.no>
2009-10-02 09:41:13 +0000

    - update info - next release's gonna be 2.0
    git-svn-id: http://svn.projects.ez.no/integrate@1666 b79a6038-f9c6-4f17-9211-4c3cdee1eac5

--- trunk/extension/integrate/ezinfo.php
+++ trunk/extension/integrate/ezinfo.php
@@ -1,13 +1,13 @@
 <?php
 class integrateInfo
 {
-    function info()
+    static function info()
     {
         return array(
             'Name' => "Integrate Datatypes",
-            'Version' => "1.0",
+            'Version' => "2.0",
             'Author' => "<a href='http://www.stuffandcontent.com'>Bruce Morrison</a>",
-            'Copyright' => "Copyright (C) 2004-2007 designIT",
+            'Copyright' => "Copyright (C) 2004-2009 designIT",
             'License' => "GNU General Public License v2.0"
         );
     }
